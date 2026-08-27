1d1b4fccafae1d76b1250146699e411aadc56f98
bansalpratham <prathambansal200@gmail.com>
2026-08-27 11:20:59 +0530

    feat: add routing and basic page structure with Home and Blog components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+
+      <Route path='/' element={<Home/>} />
+      <Route path='/blog' element={<Blog/>} />
+
+      </Routes>
     </div>
   )
 }
