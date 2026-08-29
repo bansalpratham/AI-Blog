4588a8ec305242c8c18c2114b3432a0626439ecb
bansalpratham <prathambansal200@gmail.com>
2026-08-30 03:16:04 +0530

    fix: correct script name in package.json and format console log in server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.get('/',(req,res)=>res.send("API is now working"))
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=>{
-    console.log('Server is running on port '+PORT)
+    console.log('Server is running on port '+ PORT)
 })
 
 export default app;
