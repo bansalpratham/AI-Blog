4ca560549abb4205b7f3216f1e78205c6ea03657
bansalpratham <prathambansal200@gmail.com>
2026-08-29 17:31:33 +0530

    feat: enhance Blog component with comment submission and sharing options; integrate Footer component

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -3,12 +3,16 @@ import { useParams } from 'react-router-dom'
 import { assets, blog_data , comments_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
 import Moment from 'moment'
+import Footer from '../components/Footer'
 
 function Blog() {
   const {id} = useParams()
 
   const [data,setData] = useState(null)
   const [comments,setComments] = useState([])
+
+    const [name,setName] = useState('')
+  const [content,setContent] = useState('')
 
   const fetchBlogData = async ()=>{
    const data = blog_data.find(item=>item._id === id)
@@ -17,6 +21,10 @@ function Blog() {
 
   const fetchComments = async () => {
     setComments(comments_data)
+  }
+
+  const addComment = async (e)=>{
+    e.preventDefault()
   }
 
   useEffect(()=>{
@@ -45,17 +53,46 @@ function Blog() {
 
       {/* comments section */}
       <div className='mt-14 mb-10 max-w-3xl mx-auto'>
-        <p>Comments ({comments.length}) </p>
+        <p className='font-semibold mb-4' >Comments ({comments.length}) </p>
         <div className='flex flex-col gap-4'>
             {comments.map((item,index)=>(
               <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600' >
-                <div></div>
+                <div className='flex items-center gap-2 mb-2'>
+                  <img src={assets.user_icon} alt="" className='w-6' />
+                  <p className='font-medium'>{item.name}</p>
+                </div>
+                <p className='text-sm max-w-md ml-8'>{item.content}</p>
+                <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
               </div>
             ))}
         </div>
       </div>
 
+      {/* Add Comment Section  */}
+      <div className='max-w-3xl mx-auto'>
+            <p className='font-semibold mb-4'>Add your comment</p>
+            <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg' >
+              <input onChange={(e)=>setName(e.target.value)} value={name} type="text" placeholder='Name' required className='w-full p-2 border border-gray-300 rounded outline-none'  />
+
+              <textarea onChange={(e)=>setContent(e.target.value)} value={content} placeholder='Comment' className='w-full p-2 border border-gray-300 rounded outline-none h-48' required ></textarea>
+
+              <button type='submit' className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer '>Submit</button>
+
+            </form>
+      </div>
+
+      {/* Share Buttons */}
+      <div className='my-24 max-w-3xl mx-auto'>
+              <p className='font-semibold my-4'>Share this article on social media</p>
+              <div className='flex'>
+                <img src={assets.facebook_icon} width={50} alt='' />
+                <img src={assets.twitter_icon} width={50} alt='' />
+                <img src={assets.googleplus_icon} width={50} alt='' />
+              </div>
+      </div>
+
     </div>  
+    <Footer/>
 
     </div>
   ) : <div>Loading...</div>
