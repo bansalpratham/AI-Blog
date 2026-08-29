6ebf0b24bce0fa8a49e921de262566f295ca3779
bansalpratham <prathambansal200@gmail.com>
2026-08-29 16:23:49 +0530

    feat: update routing for Blog component and add Footer and NewsLetter components to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BlogList from '../components/BlogList'
+import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
@@ -9,6 +11,8 @@ function Home() {
       <Navbar/>
       <Header/>
       <BlogList/>
+      <NewsLetter/>
+      <Footer/>
     </>
   )
 }
