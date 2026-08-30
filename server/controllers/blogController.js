26946d7275a8fd21ec5b49a4d841ea3ce8152b74
bansalpratham <prathambansal200@gmail.com>
2026-08-30 13:20:49 +0530

    feat(blog): implement addBlog function with validation for required fields

---
+++ server/controllers/blogController.js
@@ -0,0 +1,16 @@
+export const addBlog = async (req,res)=>{
+    try {
+        const {title,subTitle,description,category,isPublished} = JSON.parse(req.body.blog)
+        const imageFile = req.file
+
+        if (!title || !description || !category || !imageFile)
+        {
+            return res.json({success:fasle , missing:"Missing reuired fields"})
+        }
+
+        
+
+    } catch (error) {
+        
+    }
+}
