de025be62b84b2c596e8b42e20ef9a3f6c6b519c
bansalpratham <prathambansal200@gmail.com>
2026-09-04 18:06:27 +0530

    fix(gemini): update model version to gemini-2.5-flash in content generation

--- server/configs/gemini.js
+++ server/configs/gemini.js
@@ -7,7 +7,7 @@ const ai = new GoogleGenAI({
 const generateAIContent  = async (prompt) => {
 
     const response = await ai.models.generateContent({
-        model: "gemini-3.7-flash",
+        model: "gemini-2.5-flash",
         contents: prompt
     });
 
